1f1bc2350ffa2ffd65100742f20890302051aaaa
sean-rohan-NOAA <sean.rohan@noaa.gov>
2021-01-21 17:41:35 -0800

    Redo data

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Yellowfin sole CPUE data
+#'
+#' Yellowfin sole CPUE data from NOAA/AFSC 2017 summer bottom trawl survey of the eastern Bering Sea continental shelf.
+#'
+#' @format A data frame with 376 rows and 5 variables:
+#' \describe{
+#'   \item{LONGITUDE}{Longitude in decimal degrees}
+#'   \item{LATITUDE}{Latitude in decimal degrees}
+#'   ...
+#' }
+#' @source \url{https://www.fisheries.noaa.gov/about/alaska-fisheries-science-center}
+"YFS2017"
