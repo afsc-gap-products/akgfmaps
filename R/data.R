33bae68ca66905fda95ecdca470a5db345b248d8
sean-rohan-NOAA <sean.rohan@noaa.gov>
2021-01-21 18:00:23 -0800

    Redo data

--- R/data.R
+++ R/data.R
@@ -2,10 +2,15 @@
 #'
 #' Yellowfin sole CPUE data from NOAA/AFSC 2017 summer bottom trawl survey of the eastern Bering Sea continental shelf.
 #'
-#' @format A data frame with 376 rows and 5 variables:
+#' @format A data frame with 376 rows and y variables:
 #' \describe{
+#'   \item{COMMON_NAME}{Species common name}
+#'   \item{YEAR}{Survey year}
+#'   \item{HAUL}{Haul number}
+#'   \item{STATIONID}{Station ID}
 #'   \item{LONGITUDE}{Longitude in decimal degrees}
 #'   \item{LATITUDE}{Latitude in decimal degrees}
+#'   \item{CPUE_KGHA}{Catch per unit effort in kilograms per hectare}
 #'   ...
 #' }
 #' @source \url{https://www.fisheries.noaa.gov/about/alaska-fisheries-science-center}
