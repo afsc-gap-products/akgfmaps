b403409841dd6e324eef4632985b6a282b51b22a
Emily Ryznar <emily.ryznar@nmfs.local>
2022-04-04 13:47:59 -0800

    trying a different shp file loading strategy

--- R/get_base_layers.R
+++ R/get_base_layers.R
@@ -61,9 +61,10 @@ get_base_layers <- function(select.region,
   
   # SEBS--------------------------------------------------------------------------------------------
   if(select.region %in% c("bs.south", "sebs")) {
-    survey.area <- sf::st_read(system.file("data", "BristolBay.shp", package = "akgfmaps"), 
-                               quiet = TRUE)
+    #survey.area <- sf::st_read(system.file("data", "BristolBay.shp", package = "akgfmaps"), 
+                               #quiet = TRUE)
       #dplyr::filter(SURVEY == "EBS_SHELF")
+    survey.area<-rgdal:: readOGR(dsn="./SAP_data/BristolBay.shp")
     survey.strata <- sf::st_read(system.file("data", "ebs_strata.shp", package = "akgfmaps"), 
                                  quiet = TRUE) %>%
       dplyr::filter(Stratum %in% c(10, 20, 31, 32, 41, 42, 43, 50, 61, 62, 82, 90))
