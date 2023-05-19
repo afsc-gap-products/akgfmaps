6cb8c3638aa2f842a933768ff748b83f583ecbaa
sean-rohan-NOAA <sean.rohan@noaa.gov>
2023-05-18 18:56:11 -0700

    Update tests for terra

--- tests/testthat/test-auto-mapping.R
+++ tests/testthat/test-auto-mapping.R
@@ -4,12 +4,12 @@ testthat::test_that("Test auto-mapping functions interpolation",
                     {test_map <- suppressWarnings(make_idw_map(x = akgfmaps::YFS2017, grid.cell = c(5000, 5000)));
                     test_labs <- suppressWarnings(add_map_labels(x = test_map));
                     test_resize <- suppressWarnings(akgfmaps::change_text_size(x = test_map, size.mult = 2));
-                    test_raster_mask <- rasterize_and_mask(raster::raster(nrows = 100, 
+                    test_raster_mask <- rasterize_and_mask(terra::rast(nrows = 100, 
                                                              ncols = 100, 
-                                                             xmn = test_map$map_layers$plot.boundary$x[1],
-                                                             xmx = test_map$map_layers$plot.boundary$x[2],
-                                                             ymn = test_map$map_layers$plot.boundary$y[1],
-                                                             ymx = test_map$map_layers$plot.boundary$y[2], 
+                                                             xmin = test_map$map_layers$plot.boundary$x[1],
+                                                             xmax = test_map$map_layers$plot.boundary$x[2],
+                                                             ymin = test_map$map_layers$plot.boundary$y[1],
+                                                             ymax = test_map$map_layers$plot.boundary$y[2], 
                                                              vals = rnorm(1e4)), 
                                                            amask = test_map$map_layers$survey.area);
                     expect_equal(test_map$n.breaks[1], 6);
