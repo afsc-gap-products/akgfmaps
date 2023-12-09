25a576e05a602dea4939ea4f9f13f2c19d9ae62d
sean-rohan-NOAA <sean.rohan@noaa.gov>
2023-12-09 13:15:20 -0800

    Update built-in datasets

--- R/data.R
+++ R/data.R
@@ -1,8 +1,8 @@
-#' Yellowfin sole CPUE data
+#' EBS yellowfin sole CPUE data from 2017
 #'
 #' Yellowfin sole CPUE data from NOAA/AFSC 2017 summer bottom trawl survey of the eastern Bering Sea continental shelf.
 #'
-#' @format A data frame with 376 rows and y variables:
+#' @format A data frame with 376 rows  and 7 columns:
 #' \describe{
 #'   \item{COMMON_NAME}{Species common name}
 #'   \item{YEAR}{Survey year}
@@ -15,3 +15,39 @@
 #' }
 #' @source \url{https://www.fisheries.noaa.gov/about/alaska-fisheries-science-center}
 "YFS2017"
+
+#' EBS yellowfin sole CPUE data from 2018
+#'
+#' Yellowfin sole CPUE data from the NOAA/AFSC 2018 summer bottom trawl survey of the eastern Bering Sea continental shelf.
+#'
+#' @format A data frame with 376 rows and 7 columns:
+#' \describe{
+#'   \item{COMMON_NAME}{Species common name}
+#'   \item{YEAR}{Survey year}
+#'   \item{HAUL}{Haul number}
+#'   \item{STATIONID}{Station ID}
+#'   \item{LONGITUDE}{Longitude in decimal degrees}
+#'   \item{LATITUDE}{Latitude in decimal degrees}
+#'   \item{CPUE_KGHA}{Catch per unit effort in kilograms per hectare}
+#'   ...
+#' }
+#' @source \url{https://www.fisheries.noaa.gov/about/alaska-fisheries-science-center}
+"YFS2018"
+
+#' NBS noodle bryozoan CPUE data from 2017-2023
+#'
+#' Noodle bryozoan (Alcyonidium enteromorpha) CPUE data from the NOAA/AFSC 2017, 2019, and 2021-2023 summer bottom trawl survey of the northern Bering Sea.
+#'
+#' @format A data frame with 832 rows and 7 columns:
+#' \describe{
+#'   \item{COMMON_NAME}{Species common name}
+#'   \item{YEAR}{Survey year}
+#'   \item{HAUL}{Haul number}
+#'   \item{STATIONID}{Station ID}
+#'   \item{LONGITUDE}{Longitude in decimal degrees}
+#'   \item{LATITUDE}{Latitude in decimal degrees}
+#'   \item{CPUE_KGHA}{Catch per unit effort in kilograms per hectare}
+#'   ...
+#' }
+#' @source \url{https://www.fisheries.noaa.gov/about/alaska-fisheries-science-center}
+"NOODLES"
