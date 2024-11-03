9ef3f0187655ce53a48b0f60b0ee1a6330a0fcd0
sean-rohan-NOAA <sean.rohan@noaa.gov>
2024-11-03 10:22:35 -0800

    Drop interp dev functions and tests

--- tests/testthat/test-utilities.R
+++ tests/testthat/test-utilities.R
@@ -8,11 +8,13 @@ testthat::test_that("Test data frame CRS transformation",
 testthat::test_that("Check breaks function",
                     {expect_equal(ncol(eval_plot_breaks(CPUE = 1:20, n.breaks = 4, styles = "quantile")), 6)})
 
-testthat::test_that("Test normal score transformations",
-                    {expect_equal(suppressWarnings(backtransform_normal(scores = normal_transform(1:10)$z_score,
-                                                                        z_score = normal_transform(1:10),
-                                                                        make_plot = FALSE)), 1:10)})
-
 testthat::test_that("Check stations",
-                    {expect_equal(length(get_survey_stations(select.region = "sebs")), 376);
-                      expect_equal(length(get_survey_stations(select.region = "nbs")), 144)})
+                    {expect_equal(length(get_survey_stations(select.region = "sebs",
+                                                             include.corners = TRUE)), 376);
+                      expect_equal(length(get_survey_stations(select.region = "sebs",
+                                                              include.corners = TRUE)), 350);
+                      expect_equal(length(get_survey_stations(select.region = "nbs",
+                                                              include.corners = TRUE)), 144)})
+
+
+
