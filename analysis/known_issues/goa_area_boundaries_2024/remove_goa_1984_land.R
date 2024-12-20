8479e7483cda87ebb28ba2c2747b3cf8d8809329
Sean Rohan <sean.rohan@noaa.gov>
2024-12-20 10:25:43 -0800

    Remove land area from GOA survey area polygon

---
+++ analysis/known_issues/goa_area_boundaries_2024/remove_goa_1984_land.R
@@ -0,0 +1,12 @@
+# Remove land area from GOA survey area
+
+library(sf)
+library(dplyr)
+
+goa_area_without_land <- sf::st_read(here::here("analysis", "known_issues", "goa_area_boundaries_2024", "goa_strata.shp"), quiet = TRUE) |>
+  dplyr::filter(STRATUM > 0 & STRATUM < 800) |>
+  dplyr::group_by(REGION = "GOA") |>
+  dplyr::summarise(AREA_KM2 = sum(AREA_KM2))
+
+goa_area_without_land |>
+  sf::st_write(here::here("inst", "extdata", "goa_area.shp"))
