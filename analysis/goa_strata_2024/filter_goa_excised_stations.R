276d2a556c397365287240f9c757cd3c5ab7cea2
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2025-05-20 11:55:21 -0700

    added Oracle upload

--- analysis/goa_strata_2024/filter_goa_excised_stations.R
+++ analysis/goa_strata_2024/filter_goa_excised_stations.R
@@ -1,7 +1,7 @@
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## Project:       Excise historical GOA stations outside of main NMFS areas
 ## Author:        Zack Oyafuso (zack.oyafuso@noaa.gov)
-## Description:
+## Description:   These records will be appended to GAP_PRODUCTS tables
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ## Restart R Session before running
@@ -62,3 +62,77 @@ terra::writeVector(
   x = goa_excised, 
   filename = "analysis/goa_strata_2024/goa_excised_stations.gpkg"
 )
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Append new area records to Oracle 
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+area_1984 <- 
+  RODBC::sqlQuery(channel = channel, 
+                  query = "SELECT * FROM GAP_PRODUCTS.AREA
+                           WHERE SURVEY_DEFINITION_ID = 47
+                           AND DESIGN_YEAR = 1984")
+goa_strata_2024$AREA_KM2 <- goa_strata_2024$AREA_M2 * 1e-6
+area_2024 <- merge(x = subset(x = area_1984, select = -AREA_KM2), 
+                   by.x = "AREA_ID", all.x = TRUE, 
+                   y = subset(x = as.data.frame(x = goa_strata_2024), 
+                              select = c("STRATUM", "AREA_KM2")),
+                   by.y = "STRATUM")
+area_2024$DESIGN_YEAR <- 2024
+area_2024 <- area_2024[, names(x = area_1984)]
+
+col_metadata <- 
+  RODBC::sqlQuery(channel = channel, 
+                  query = paste('
+                SELECT METADATA_COLNAME as "colname", 
+                METADATA_DATATYPE as "datatype"
+                FROM GAP_PRODUCTS.METADATA_COLUMN
+                         WHERE METADATA_COLNAME IN',
+                                gapindex::stitch_entries(names(x = area_2024)))
+  )
+
+RODBC::sqlSave(
+  channel = channel,
+  dat = as.data.frame(x = area_2024),
+  tablename = "GAP_PRODUCTS.AREA",
+  append = TRUE,
+  rownames = FALSE,
+  varTypes = stats::setNames(object = col_metadata$datatype,
+                             nm = col_metadata$colname)[names(area_2024)]
+)
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Append new stratum_groups records to Oracle
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+stratum_groups_2024 <- 
+  RODBC::sqlQuery(channel = channel, 
+                  query = "SELECT * FROM GAP_PRODUCTS.STRATUM_GROUPS
+                         WHERE SURVEY_DEFINITION_ID = 47
+                         AND DESIGN_YEAR = 1984")
+stratum_groups_2024$DESIGN_YEAR <- 2024
+
+col_metadata <- 
+  RODBC::sqlQuery(
+    channel = channel, 
+    query = paste('
+                SELECT METADATA_COLNAME as "colname", 
+                METADATA_DATATYPE as "datatype"
+                FROM GAP_PRODUCTS.METADATA_COLUMN
+                WHERE METADATA_COLNAME IN',
+                  gapindex::stitch_entries(names(x = stratum_groups_2024)))
+  )
+
+RODBC::sqlSave(
+  channel = channel,
+  dat = stratum_groups_2024,
+  tablename = "GAP_PRODUCTS.STRATUM_GROUPS",
+  append = TRUE,
+  rownames = FALSE,
+  varTypes = stats::setNames(object = col_metadata$datatype,
+                             nm = col_metadata$colname)[names(x = stratum_groups_2024)]
+)
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   GAP_PRODUCTS.SURVEY_DESIGN was modified by hand in SQL Developer
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
